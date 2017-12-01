aa2a340a41af36faf6748fb24a3a3067d144f61e
Sunil Boga <30490515+boga5@users.noreply.github.com>
2017-12-01 12:20:15 +0530

    Update robot.sh

--- robot/robot.sh
+++ robot/robot.sh
@@ -2,4 +2,4 @@ echo "Execute robot framework: Connect to Oracle, test on sample project service
 HOSTNAME=$1
 PORT=$2
 DBURL=$3
-pybot --variable hostname:${HOSTNAME} --variable port:${PORT} --variable dbConnectionStr:${DBURL} ../tests/sampletest.robot
+pybot --variable hostname:${HOSTNAME} --variable port:${PORT} --variable dbConnectionStr:${DBURL} ./robot/tests/sampletest.robot
